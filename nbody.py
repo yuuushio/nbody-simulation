5dd0314cc8097999100786ce7916ab20649ce5be
yuuushio <>
2022-04-29 08:05:32 +1000

    create body class

--- nbody.py
+++ nbody.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+class Body:
+    def __init__(self,x,y,z,vel_x,vel_y,vel_z,mass):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.vel_z = vel_z
+        self.mass = mass
