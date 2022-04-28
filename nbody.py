45c40b2cf614b7b38d1f6730dc01391f19f6dd07
yuuushio <>
2022-04-29 08:13:29 +1000

    define base constants

--- nbody.py
+++ nbody.py
@@ -9,3 +9,15 @@ class Body:
         self.vel_y = vel_y
         self.vel_z = vel_z
         self.mass = mass
+
+def pi_macro():
+    return 3.141592653589793
+
+def solar_mass():
+    return (4*pi_macro()*pi_macro())
+
+def n_day():
+    return 365.25
+
+def g_const():
+    return (6.67e-11)
