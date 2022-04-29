c48adfa04cc9ef2a7f6d049ee8f3fced6e1b239d
yuuushio <>
2022-04-29 11:13:53 +1000

    calc acceleration, update vel, update, pos

--- nbody.py
+++ nbody.py
@@ -26,19 +26,38 @@ def calc_distance(b_a, b_b):
     dx = b_a.x - b_b.x
     dy = b_a.y - b_b.y
     dz = b_a.z - b_b.z
-    return np.sqrt(np.power(dx, 2) + np.power(dy, 2) + np.power(dz, 2))
+    delta_d = [dx, dy, dz]
+    return np.sqrt(np.power(dx, 2) + np.power(dy, 2) + np.power(dz, 2)), delta_d
 
 def calc_magnitude(a, b, distance):
     mass_product = a.mass*b.mass
     dist_sq = np.power(distance, 2)
     return g_const()*(mass_product/dist_sq)
 
+def unit_vec(delta_d, dist):
+    uv = [delta_d[0]/dist, delta_d[1]/dist, delta_d[2]/dist]
+    return uv
+
 # Advances the simulation with time difference (dt)
 def step(body_list, dt):
     for i in range(len(body_list)):
+        a_x = 0
+        a_y = 0
+        a_z = 0
         for j in range(len(body_list)):
-            distance = calc_distance(body_list[i], body_list[j])
+            if i ==j: continue
+            distance, delta_d = calc_distance(body_list[i], body_list[j])
             magnitude = calc_magnitude(body_list[i], body_list[j], distance)
+            unit_v = unit_vec(delta_d, distance)
+            a_x += (unit_v[0]*magnitude)/body_list[i].mass
+            a_y += (unit_v[1]*magnitude)/body_list[i].mass
+            a_z += (unit_v[2]*magnitude)/body_list[i].mass
+        body_list[i].vel_x += a_x*dt
+        body_list[i].vel_y += a_y*dt
+        body_list[i].vel_z += a_z*dt
+        body_list[i].x += body_list[i].vel_x*dt
+        body_list[i].y += body_list[i].vel_y*dt
+        body_list[i].z += body_list[i].vel_z*dt
 
 def main():
     body_list = []
